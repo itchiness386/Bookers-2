743387dafddbe246fec6a88711142a8dd1853637
itchiness386 <itchiness386@gmail.com>
2020-01-18 18:55:13 +0900

    [Add] Bookモデル・Aboutページ・ヘッダーのリンク(Books/About)

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,11 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  get "home/about"
   devise_for :users
-  root 'users#show'
   resources :users, only: [:index, :show, :edit]
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
