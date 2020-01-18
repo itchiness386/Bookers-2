2797b96a6bc4528a1a282ecb10c6f4e03b951d10
itchiness386 <itchiness386@gmail.com>
2020-01-18 17:47:07 +0900

    [Add] ユーザー名でのログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,20 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
 
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :name, :email, :password, :password_confirmation, :profile_image_id, :introduction ]
-    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_image_id, :introduction])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
+    added_attrs = [ :email, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 
 end
