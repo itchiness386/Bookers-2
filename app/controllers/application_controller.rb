f1385bbc5c8bf80d48bde87a5c36dd05537cf528
itchiness386 <itchiness386@gmail.com>
2020-01-18 16:48:08 +0900

    [Add] ヘッダー・フッター・ユーザー一覧ページ・画像(no_image.jpg)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :email, :name, :password, :password_confirmation ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    added_attrs = [ :name, :email, :password, :password_confirmation, :profile_image_id, :introduction ]
+    devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_image_id, :introduction])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
   end
 
 end
