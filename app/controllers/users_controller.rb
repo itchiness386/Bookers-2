ed7c4522074fc953737935935c6447036267b4d7
itchiness386 <itchiness386@gmail.com>
2020-01-20 17:32:51 +0900

    [Update] レイアウト調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,34 @@
 class UsersController < ApplicationController
+
   before_action :authenticate_user!
+
   def index
-  	@users = User.all
+    @users = User.all
   end
+
   def show
-  	@user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
   end
+
   def edit
-  	@user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
+
+  def update
+    @user = User.find(current_user.id)
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
+
+  def userinfo
+    @user = User.find(current_user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
