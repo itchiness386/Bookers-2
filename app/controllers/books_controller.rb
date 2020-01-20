47c2dfa1364c7b87940c647e17740462cef08bdd
itchiness386 <itchiness386@gmail.com>
2020-01-20 17:32:51 +0900

    [Update] レイアウト調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,43 @@
 class BooksController < ApplicationController
+
   before_action :authenticate_user!
+
   def index
+  	@books = Book.all
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	@book.save
+  	redirect_to book_path(@book.id)
   end
 
   def show
+  	@book = Book.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+
+  def update
+  	@book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+  end
+
+  def newbook
+    @book = Book.new
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
