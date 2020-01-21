adf30735ae3697e278d13e0d5763341bceb7b3cc
itchiness386 <itchiness386@gmail.com>
2020-01-21 21:05:36 +0900

    [Add] ユーザー画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3c1fa622afadbe9631d4117298630954ec1714fc1c2d5da55bbc1bcc0599e1d89f5783bfc43d7dbc7f78f060e2aa817a6247a7dd97dd6431d97b4c682fc2af2d'
