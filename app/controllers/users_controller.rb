5ade410f3585fc0f69d1630ec87dcacb3dff737b
itchiness386 <itchiness386@gmail.com>
2020-01-21 21:05:36 +0900

    [Add] ユーザー画像投稿機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(current_user.id)
+    redirect_to user_path(@user.id)
   end
 
   def userinfo
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get "home/about"
   devise_for :users
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
